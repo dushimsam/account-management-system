ecdc878222d021191a392101bc32d8427f3dadcd
dushimsam <dushsam@gmail.com>
2023-04-24 17:08:31 +0200

    ch: initial backend setup

---
+++ accountmanagementsystem/src/main/java/com/companyz/accountmanagementsystem/AccountmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.companyz.accountmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountmanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountmanagementsystemApplication.class, args);
+	}
+
+}
