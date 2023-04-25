9cba71074e1643080b9d39dd42580bfa697b061e
dushimsam <dushsam@gmail.com>
2023-04-25 22:07:24 +0200

    feat(DTO): added initiate verification dto object

---
+++ accountmanagementsystem/src/main/java/com/companyz/accountmanagementsystem/dto/accountverificationdto/InitiateVerificationDto.java
@@ -0,0 +1,19 @@
+package com.companyz.accountmanagementsystem.dto.accountverificationdto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class InitiateVerificationDto {
+
+    @NotNull
+    private String nidOrPassport;
+
+    @NotNull
+    private Long userId;
+}
