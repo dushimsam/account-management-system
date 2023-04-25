bd087f7011accc7c84262ddffe421435ed4cc863
dushimsam <dushsam@gmail.com>
2023-04-25 22:05:50 +0200

    feat(DTO): added a global dto object

---
+++ accountmanagementsystem/src/main/java/com/companyz/accountmanagementsystem/dto/GlobalDtoGet.java
@@ -0,0 +1,12 @@
+package com.companyz.accountmanagementsystem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GlobalDtoGet {
+    private String message;
+}
